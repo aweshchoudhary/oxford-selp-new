08869c7113b56dce9e77690ac6dd6c11cd1827b7
Awesh Choudhary <aweshchoudhary7@gmail.com>
2024-07-24 16:38:30 +0530

    updated

--- footer.php
+++ footer.php
@@ -1,119 +1,67 @@
 </main>
-<footer class="md:px-44 p-5 md:py-10 bg-primary text-white flex flex-wrap gap-5">
-    <div class="md:basis-1/4 basis-full flex-1 gap-5">
-        <figure class="md:w-1/2 w-20">
-            <?php echo wp_get_attachment_image(468, "medium", false, [
-                "loading" => "lazy",
-                "class" => "image-contain",
-            ]); ?>
-            <figcaption><?php echo wp_get_attachment_caption(
-                            468
-                        ); ?></figcaption>
-            <figcaption class="sr-only">xed footer logo white</figcaption>
-        </figure>
+<footer class="section bg-primary text-white">
+    <div class="flex flex-wrap lg:gap-20 md:gap-10 gap-5">
+        <div class="shrink-0">
+            <figure class="md:w-32 w-20">
+                <?php $image_id = get_field("footer_logo", "option") ?>
+                <?php echo wp_get_attachment_image($image_id, "medium", false, [
+                    "loading" => "lazy",
+                    "class" => "image-contain",
+                ]); ?>
+                <figcaption><?php echo wp_get_attachment_caption(
+                                $image_id
+                            ); ?></figcaption>
+            </figure>
 
-        <ul class="list-none pl-0 mt-10 space-y-3 text-lg">
-            <li class="flex items-center gap-5">
-                <svg xmlns="http://www.w3.org/2000/svg" class="shrink-0 md:text-2xl text-xl" width="1em" height="1em" class="text-3xl" viewBox="0 0 24 24">
-                    <path fill="currentColor" d="M19.95 21q-3.125 0-6.175-1.362t-5.55-3.863t-3.862-5.55T3 4.05q0-.45.3-.75t.75-.3H8.1q.35 0 .625.238t.325.562l.65 3.5q.05.4-.025.675T9.4 8.45L6.975 10.9q.5.925 1.187 1.787t1.513 1.663q.775.775 1.625 1.438T13.1 17l2.35-2.35q.225-.225.588-.337t.712-.063l3.45.7q.35.1.575.363T21 15.9v4.05q0 .45-.3.75t-.75.3" />
-                </svg>
-                (Global): +1 6312 573288
-            </li>
-            <li class="flex items-center gap-5">
-                <svg xmlns="http://www.w3.org/2000/svg" class="shrink-0 md:text-2xl text-xl" width="1em" height="1em" class="text-3xl" viewBox="0 0 24 24">
-                    <path fill="currentColor" d="M19.95 21q-3.125 0-6.175-1.362t-5.55-3.863t-3.862-5.55T3 4.05q0-.45.3-.75t.75-.3H8.1q.35 0 .625.238t.325.562l.65 3.5q.05.4-.025.675T9.4 8.45L6.975 10.9q.5.925 1.187 1.787t1.513 1.663q.775.775 1.625 1.438T13.1 17l2.35-2.35q.225-.225.588-.337t.712-.063l3.45.7q.35.1.575.363T21 15.9v4.05q0 .45-.3.75t-.75.3" />
-                </svg>
-                (USA): +1 6463 184291
-            </li>
-            <li class="flex items-center gap-5">
-                <svg xmlns="http://www.w3.org/2000/svg" class="shrink-0 md:text-2xl text-xl" width="1em" height="1em" class="text-3xl" viewBox="0 0 24 24">
-                    <path fill="currentColor" d="M19.95 21q-3.125 0-6.175-1.362t-5.55-3.863t-3.862-5.55T3 4.05q0-.45.3-.75t.75-.3H8.1q.35 0 .625.238t.325.562l.65 3.5q.05.4-.025.675T9.4 8.45L6.975 10.9q.5.925 1.187 1.787t1.513 1.663q.775.775 1.625 1.438T13.1 17l2.35-2.35q.225-.225.588-.337t.712-.063l3.45.7q.35.1.575.363T21 15.9v4.05q0 .45-.3.75t-.75.3" />
-                </svg>
-                (Singapore): +65 3138 5286
-            </li>
-            <li class="flex items-center gap-5">
-                <svg xmlns="http://www.w3.org/2000/svg" class="shrink-0 md:text-2xl text-xl" width="1em" height="1em" class="text-3xl" viewBox="0 0 24 24">
-                    <path fill="currentColor" d="M19.95 21q-3.125 0-6.175-1.362t-5.55-3.863t-3.862-5.55T3 4.05q0-.45.3-.75t.75-.3H8.1q.35 0 .625.238t.325.562l.65 3.5q.05.4-.025.675T9.4 8.45L6.975 10.9q.5.925 1.187 1.787t1.513 1.663q.775.775 1.625 1.438T13.1 17l2.35-2.35q.225-.225.588-.337t.712-.063l3.45.7q.35.1.575.363T21 15.9v4.05q0 .45-.3.75t-.75.3" />
-                </svg>
-                (UAE): +97 1501 227980
-            </li>
-            <li class="flex items-center gap-5">
-                <svg xmlns="http://www.w3.org/2000/svg" class="shrink-0 md:text-2xl text-xl" width="1em" height="1em" class="text-3xl" viewBox="0 0 24 24">
-                    <path fill="currentColor" d="M19.95 21q-3.125 0-6.175-1.362t-5.55-3.863t-3.862-5.55T3 4.05q0-.45.3-.75t.75-.3H8.1q.35 0 .625.238t.325.562l.65 3.5q.05.4-.025.675T9.4 8.45L6.975 10.9q.5.925 1.187 1.787t1.513 1.663q.775.775 1.625 1.438T13.1 17l2.35-2.35q.225-.225.588-.337t.712-.063l3.45.7q.35.1.575.363T21 15.9v4.05q0 .45-.3.75t-.75.3" />
-                </svg>
-                (India): +91 7702 778916
-            </li>
-            <li class="flex items-center gap-5">
-                <svg xmlns="http://www.w3.org/2000/svg" class="shrink-0 md:text-2xl text-xl" width="1em" height="1em" class="text-3xl" viewBox="0 0 20 20">
-                    <path fill="currentColor" d="M18 7.373V14.5a2.5 2.5 0 0 1-2.5 2.5h-11A2.5 2.5 0 0 1 2 14.5V7.373l7.746 4.558a.5.5 0 0 0 .508 0zM15.5 4a2.5 2.5 0 0 1 2.485 2.223L10 10.92L2.015 6.223A2.5 2.5 0 0 1 4.5 4z" />
-                </svg>
-                oxford.selp@xedinstitute.org
-            </li>
-        </ul>
-    </div>
-    <div class="md:basis-1/4 basis-full flex-1 gap-5">
-        <div class="flex items-start md:gap-5 gap-3 md:mt-10 mt-5">
-            <div>
-                <svg xmlns="http://www.w3.org/2000/svg" class="md:text-3xl text-2xl shrink-0" width="1.13em" height="1em" viewBox="0 0 576 512">
-                    <path fill="currentColor" d="M280.37 148.26L96 300.11V464a16 16 0 0 0 16 16l112.06-.29a16 16 0 0 0 15.92-16V368a16 16 0 0 1 16-16h64a16 16 0 0 1 16 16v95.64a16 16 0 0 0 16 16.05L464 480a16 16 0 0 0 16-16V300L295.67 148.26a12.19 12.19 0 0 0-15.3 0M571.6 251.47L488 182.56V44.05a12 12 0 0 0-12-12h-56a12 12 0 0 0-12 12v72.61L318.47 43a48 48 0 0 0-61 0L4.34 251.47a12 12 0 0 0-1.6 16.9l25.5 31A12 12 0 0 0 45.15 301l235.22-193.74a12.19 12.19 0 0 1 15.3 0L530.9 301a12 12 0 0 0 16.9-1.6l25.5-31a12 12 0 0 0-1.7-16.93" />
-                </svg>
-            </div>
-            <div>
-                <h3 class="text-2xl mb-1 font-semibold">Singapore</h3>
-                <p>80 Robinson Road #15-02, <br>
-                    Singapore, 068898 </p>
-            </div>
+            <ul class="list-none pl-0 mt-10 space-y-3 text-lg">
+                <?php
+                $contacts = get_field("contacts", "option");
+                if (count($contacts)) :
+                    foreach ($contacts as $item) :
+                ?>
+                        <li>
+                            <a href="<?php echo $item["link"]["url"] ?>" class="text-white hover:text-white/80 flex items-center gap-5" style="color: #FFF;">
+                                <figure class="w-[20px]">
+                                    <?php echo wp_get_attachment_image($item["icon"], "thumbnail", false, [
+                                        "loading" => "lazy",
+                                        "class" => "image-contain",
+                                    ]); ?>
+                                    <figcaption><?php echo wp_get_attachment_caption(
+                                                    $item["icon"]
+                                                ); ?></figcaption>
+                                </figure>
+                                <?php echo $item["link"]["title"] ?>
+                            </a>
+                        </li>
+                <?php endforeach;
+                endif;
+                ?>
+            </ul>
         </div>
-        <div class="flex items-start md:gap-5 gap-3 md:mt-10 mt-5">
-            <div>
-                <svg xmlns="http://www.w3.org/2000/svg" class="md:text-3xl text-2xl shrink-0" width="1.13em" height="1em" viewBox="0 0 576 512">
-                    <path fill="currentColor" d="M280.37 148.26L96 300.11V464a16 16 0 0 0 16 16l112.06-.29a16 16 0 0 0 15.92-16V368a16 16 0 0 1 16-16h64a16 16 0 0 1 16 16v95.64a16 16 0 0 0 16 16.05L464 480a16 16 0 0 0 16-16V300L295.67 148.26a12.19 12.19 0 0 0-15.3 0M571.6 251.47L488 182.56V44.05a12 12 0 0 0-12-12h-56a12 12 0 0 0-12 12v72.61L318.47 43a48 48 0 0 0-61 0L4.34 251.47a12 12 0 0 0-1.6 16.9l25.5 31A12 12 0 0 0 45.15 301l235.22-193.74a12.19 12.19 0 0 1 15.3 0L530.9 301a12 12 0 0 0 16.9-1.6l25.5-31a12 12 0 0 0-1.7-16.93" />
-                </svg>
-            </div>
-            <div>
-                <h3 class="text-2xl mb-1 font-semibold">USA</h3>
-                <p>146 W 29th ST New York, <br>
-                    NY 10001 </p>
-            </div>
-        </div>
-        <div class="flex items-start md:gap-5 gap-3 md:mt-10 mt-5">
-            <div>
-                <svg xmlns="http://www.w3.org/2000/svg" class="md:text-3xl text-2xl shrink-0" width="1.13em" height="1em" viewBox="0 0 576 512">
-                    <path fill="currentColor" d="M280.37 148.26L96 300.11V464a16 16 0 0 0 16 16l112.06-.29a16 16 0 0 0 15.92-16V368a16 16 0 0 1 16-16h64a16 16 0 0 1 16 16v95.64a16 16 0 0 0 16 16.05L464 480a16 16 0 0 0 16-16V300L295.67 148.26a12.19 12.19 0 0 0-15.3 0M571.6 251.47L488 182.56V44.05a12 12 0 0 0-12-12h-56a12 12 0 0 0-12 12v72.61L318.47 43a48 48 0 0 0-61 0L4.34 251.47a12 12 0 0 0-1.6 16.9l25.5 31A12 12 0 0 0 45.15 301l235.22-193.74a12.19 12.19 0 0 1 15.3 0L530.9 301a12 12 0 0 0 16.9-1.6l25.5-31a12 12 0 0 0-1.7-16.93" />
-                </svg>
-            </div>
-            <div>
-                <h3 class="text-2xl mb-1 font-semibold">Saudi Arabia</h3>
-                <p>Le Cygne Commercial Center 2,
-                    6th Floor Al-Arz,
-                    Al-Olaya, Riyadh 12611 </p>
-            </div>
-        </div>
-    </div>
-    <div class="md:basis-1/4 basis-full flex-1 gap-5">
-        <div class="flex items-start md:gap-5 gap-3 md:mt-10 mt-5">
-            <div>
-                <svg xmlns="http://www.w3.org/2000/svg" class="md:text-3xl text-2xl shrink-0" width="1.13em" height="1em" viewBox="0 0 576 512">
-                    <path fill="currentColor" d="M280.37 148.26L96 300.11V464a16 16 0 0 0 16 16l112.06-.29a16 16 0 0 0 15.92-16V368a16 16 0 0 1 16-16h64a16 16 0 0 1 16 16v95.64a16 16 0 0 0 16 16.05L464 480a16 16 0 0 0 16-16V300L295.67 148.26a12.19 12.19 0 0 0-15.3 0M571.6 251.47L488 182.56V44.05a12 12 0 0 0-12-12h-56a12 12 0 0 0-12 12v72.61L318.47 43a48 48 0 0 0-61 0L4.34 251.47a12 12 0 0 0-1.6 16.9l25.5 31A12 12 0 0 0 45.15 301l235.22-193.74a12.19 12.19 0 0 1 15.3 0L530.9 301a12 12 0 0 0 16.9-1.6l25.5-31a12 12 0 0 0-1.7-16.93" />
-                </svg>
-            </div>
-            <div>
-                <h3 class="text-2xl mb-1 font-semibold">UAE</h3>
-                <p>Al Sawari Tower - B, National Theatre Airport Road, Al Khalidiya, Abu Dhabi City, Abu Dhabi</p>
-            </div>
-        </div>
-        <div class="flex items-start md:gap-5 gap-3 md:mt-10 mt-5">
-            <div>
-                <svg xmlns="http://www.w3.org/2000/svg" class="md:text-3xl text-2xl shrink-0" width="1.13em" height="1em" viewBox="0 0 576 512">
-                    <path fill="currentColor" d="M280.37 148.26L96 300.11V464a16 16 0 0 0 16 16l112.06-.29a16 16 0 0 0 15.92-16V368a16 16 0 0 1 16-16h64a16 16 0 0 1 16 16v95.64a16 16 0 0 0 16 16.05L464 480a16 16 0 0 0 16-16V300L295.67 148.26a12.19 12.19 0 0 0-15.3 0M571.6 251.47L488 182.56V44.05a12 12 0 0 0-12-12h-56a12 12 0 0 0-12 12v72.61L318.47 43a48 48 0 0 0-61 0L4.34 251.47a12 12 0 0 0-1.6 16.9l25.5 31A12 12 0 0 0 45.15 301l235.22-193.74a12.19 12.19 0 0 1 15.3 0L530.9 301a12 12 0 0 0 16.9-1.6l25.5-31a12 12 0 0 0-1.7-16.93" />
-                </svg>
-            </div>
-            <div>
-                <h3 class="text-2xl mb-1 font-semibold">India</h3>
-                <p>7th Floor, Capital Building, G-Block,
-                    Bandra Kurla Complex, Bandra (E),
-                    Mumbai 400051 </p>
-            </div>
+        <div class="flex flex-wrap flex-1">
+            <?php
+            $addresses = get_field("addresses", "option");
+            if (count($addresses)) :
+                foreach ($addresses as $item) :
+            ?>
+                    <div class="md:basis-1/2 basis-full flex items-start md:gap-5 gap-3 md:mt-10 mt-5">
+                        <figure class="md:w-[40px] w-[30px] shrink-0">
+                            <?php echo wp_get_attachment_image($item["icon"], "thumbnail", false, [
+                                "loading" => "lazy",
+                                "class" => "image-contain",
+                            ]); ?>
+                            <figcaption><?php echo wp_get_attachment_caption(
+                                            $item["icon"]
+                                        ); ?></figcaption>
+                        </figure>
+                        <div>
+                            <h3 class="text-2xl mb-1 font-semibold"><?php echo wp_kses_post($item["title"]) ?></h3>
+                            <?php echo wp_kses_post($item["address"]) ?>
+                        </div>
+                    </div>
+            <?php endforeach;
+            endif;
+            ?>
         </div>
     </div>
     <div class="md:mt-10 mt-5">
